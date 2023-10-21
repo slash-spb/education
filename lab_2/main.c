ae0670b4d864a2adb0fd18d8e0e586324c55a7ca
Александра Нагорская <a.nagorskaya@icloud.com>
2023-10-21 23:44:28 +0300

    добавила массивы с алфавитом

--- lab_2/main.c
+++ lab_2/main.c
@@ -3,6 +3,8 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    char str[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    char str_1[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+
 }
