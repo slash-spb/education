c500e9e7f0fe7140be524058704816d31c587fa4
Александра Нагорская <a.nagorskaya@icloud.com>
2023-10-22 00:36:16 +0300

    буквы в алфавитном порядке

--- lab_2/main.c
+++ lab_2/main.c
@@ -5,6 +5,22 @@ int main()
 {
     char str[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     char str_1[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char none[100];
+    char get;
+    int i = 0, a, b;
+    printf("Write a string:\n");
+    while ((get = getchar()) != '\n' && i < 99) {
+        none[i] = get;
+        i++;
+    }
+    for (a = 0; a <= 25; a++){
+        for (b = 0; b < i; b++){
+            if (str[a] == none[b]){
+                printf("%c", str[a]);
+            }
+        }
+    }
+
 
 
 }
